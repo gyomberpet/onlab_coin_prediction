13e83bb8ad92ad675e425aa4e132e749c2f0f557
Gyömbér Péter <gyomberpeti@edu.bme.hu>
2022-03-19 19:01:29 +0100

    start

---
+++ CoinPrediction/CoinPrediction/Controllers/CoinGeckoController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoinPrediction.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoinGeckoController : ControllerBase { 
+
+    }
+}
